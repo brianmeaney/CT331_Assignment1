ddd9a3a6f222a094150a931bb78d0991729c6524
brianmeaney <brianmeaney98@gmail.com>
2018-10-12 22:15:36 +0100

    edited with answer

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+  printf("Int: %d\n", sizeof(int));
+  printf("Int Pointer: %d\n", sizeof(int*));
+  printf("Long: %d\n", sizeof(long));
+  printf("Double Pointer: %d\n", sizeof(double*));
+  printf("Char Double Pointer: %d\n", sizeof(char**));
   return 0;
 }
