a03ce8ec51cea4d1eb34a8794b68dd4f02349840
brianmeaney <brianmeaney98@gmail.com>
2018-10-25 10:39:00 +0100

    created functions for tests

--- src/q2/tests.c
+++ src/q2/tests.c
@@ -2,23 +2,23 @@
 #include "tests.h"
 #include "linkedList.h"
 
-void runTests(){
+void testStack(){
 	//testing stack - push and pop
-  printf("Tests running...\n");
   printf("TESTING STACK\n");
   listElement* k = NULL;   //create blank element
   push(&k, "HEY", 30);  //push values
   push(&k, "YOU", 30);
   push(&k, "THERE", 30);
   traverse(k);
-  printf("Length: %d", length(k));
+  printf("Length: %d\n", length(k));
   printf("TESTING POP\n");
   *pop(&k); 
   traverse(k);
   printf("Length: %d", length(k));
-  
-  
-  //tetsting queue - enqueue and dequeue
+}
+
+void testQueue(){
+	  //tetsting queue - enqueue and dequeue
   printf("\n\nTESTING QUEUE\n");
   listElement* q = NULL;
   enqueue(&q, "FIRST", 30);
@@ -30,6 +30,14 @@ void runTests(){
   dequeue(q);
   traverse(q);
   printf("Length: %d", length(q));
+}
 
-  printf("\nTests complete.\n");
+void runTests(){
+	printf("Tests running...\n");
+  
+	testStack();
+	testQueue();
+
+
+	printf("\nTests complete.\n");
 }
